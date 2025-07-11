b72717c06a31ee29f747206175dd27222667540f
MatthieuDMC <matthieu.dimitrijevic@novelic.com>
2025-07-11 18:45:38 +0200

    Gazebo-ROS2 linked, Lidar OK, impossible yo move the robot, looking for solutions but not found

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -20,8 +20,8 @@ def generate_launch_description():
     pkg_path = os.path.join(get_package_share_directory('amr'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     # robot_description_config = xacro.process_file(xacro_file).toxml()
-    robot_description_config = Command(['xacro', xacro_file, 'use_ros2_control:=', use_ros2_control])
-
+    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
+    
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
@@ -31,17 +31,17 @@ def generate_launch_description():
         parameters=[params]
     )
 
+
     # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
             description='Use sim time if true'),
-        
         DeclareLaunchArgument(
             'use_ros2_control',
             default_value='true',
             description='Use ros2_control if true'),
 
-        node_robot_state_publisher,
+        node_robot_state_publisher
     ])
